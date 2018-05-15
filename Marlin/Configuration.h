23009e710ab02c09e1464ad7999961d72803f0e7
CodeMangler <hsdpal@gmail.com>
2018-05-16 05:12:46 +0530

    Update machine size to account for latest endstop and bed mods

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -791,8 +791,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 180
-#define Y_BED_SIZE 160
+#define X_BED_SIZE 200
+#define Y_BED_SIZE 140
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
